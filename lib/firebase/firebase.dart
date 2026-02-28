8109481be0ca9d2d82d6db8a99ee2ae297bc86ad
alaahamd2020 <75167389+alaahamd2020@users.noreply.github.com>
2026-02-28 04:09:17 +0300

    .

--- lib/firebase/firebase.dart
+++ lib/firebase/firebase.dart
@@ -5,34 +5,31 @@ import 'package:habits/model/habits_model.dart';
 import 'package:habits/model/topic_model.dart';
 
 class FB {
-
-  static final CollectionReference _users = FirebaseFirestore
-      .instance
+  static final CollectionReference _users = FirebaseFirestore.instance
       .collection('Users');
-  static  CollectionReference _topicsReference() => _users
-      .doc(FBAuth.user!.id!)
-      .collection('topics');
+  static CollectionReference _topicsReference() =>
+      _users.doc(FBAuth.user!.id!).collection('topics');
 
-  static  CollectionReference _habitsDataReference(String topicId) => _topicsReference()
-      .doc(topicId)
-      .collection('Habits');
+  static CollectionReference _habitsDataReference(String topicId) =>
+      _topicsReference().doc(topicId).collection('Habits');
 
-  static Stream<List<HabitsModel>> allHabits( String topicId) =>
-      _habitsDataReference( topicId).snapshots().map((event) {
+  static Stream<List<HabitsModel>> allHabits(String topicId) =>
+      _habitsDataReference(topicId).snapshots().map((event) {
         return event.docs
             .map((e) => HabitsModel.fromJson(e.data() as Map<String, dynamic>))
-            .toList().. sort((a, b) => a.date.compareTo(b.date)); 
+            .toList()
+          ..sort((a, b) => a.date.compareTo(b.date));
       });
 
   static Future<void> addHabit(HabitsModel model) async {
-    final list = await allHabits( model.topicId).first;
+    final list = await allHabits(model.topicId).first;
     final index = list.indexOf(model);
     final oldItem = index != -1 ? list[index] : null;
     model.times += oldItem?.times ?? 0;
     model.streak += oldItem?.streak ?? 0;
 
-    final doc = _habitsDataReference( model.topicId).doc(oldItem?.id);
-    if(model.id.isEmpty) model.id = doc.id;
+    final doc = _habitsDataReference(model.topicId).doc(oldItem?.id);
+    if (model.id.isEmpty) model.id = doc.id;
     await doc.set(model.toJson());
   }
 
@@ -45,16 +42,17 @@ class FB {
     topicModel.id ??= doc.id;
     doc.set(topicModel.toJson());
   }
+
   static get allTopics {
     return _topicsReference().snapshots().map((event) {
       return event.docs
           .map((e) => TopicModel.fromJson(e.data() as Map<String, dynamic>))
-          .toList()..sort((a, b) => a.createdAt.compareTo(b.createdAt));
+          .toList()
+        ..sort((a, b) => a.createdAt.compareTo(b.createdAt));
     });
   }
 
   static void deleteTopic(TopicModel topic) {
     _topicsReference().doc(topic.id).delete();
   }
-
 }
